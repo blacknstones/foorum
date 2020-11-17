28b458535c27c79a5f32db8ce12aae3cca354bda
blacknstones <blacknstones@gmail.com>
2020-11-17 17:23:26 +0100

    Minor change in components

--- frontend/src/components/posts/PostsPage.js
+++ frontend/src/components/posts/PostsPage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PostForm from "./PostForm";
 import PostCard from "./PostCard";
+import Api from "../../api/Api";
+import PostsApi from "../../api/PostsApi";
+
 function PostsPage() {
+    const createPost = (postData) => {
+        Api.post("/posts", postData)
+            .then(r => console.log(r));
+    }
+
     return (
         <div>
-            <PostForm />
+            <PostForm onSubmit = {createPost} />
             <PostCard />
         </div>
   
